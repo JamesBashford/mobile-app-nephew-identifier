dae187a510ee1eaa7c4d1eea648a24a9319c039b
JamesBashford <66199723+JamesBashford@users.noreply.github.com>
2022-03-21 20:06:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+pip install python-telegram-bot
+
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from fastai.vision.all import load_learner
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    update.message.reply_text(
+        "Bot by Bashy \n\n "
+        "Just send me a photo of Mikey or Danny and I will tell you which it is 😏"
+    )
+
+
+def help_command(update, context):
+        update.message.reply_text('My only purpose is to tell you if a picture is of Mikey or Danny. Send a photo')
+
+
+
+# def echo(update, context):
+#     print(update)
+#     print(context)
+#     update.message.reply_text(update.message.text)
+
+
+def load_model():
+    global model
+    model = load_learner('model/Mikey_Danny.pkl')
+    print('Model loaded')
+
+
+def detect_mask(update, context):
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+
+    label = model.predict('user_photo.jpg')[0]
+    if label == "mikey":
+        update.message.reply_text(
+            "Oh yeah....that's a Marvellous Mikey alright!😉"
+        )
+    else:
+        update.message.reply_text(
+            "Clearly a Daniel Monster"
+        )
+
+
+def main():
+    load_model()
+    updater = Updater(token="yourtoken", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    dp.add_handler(MessageHandler(Filters.photo, detect_mask))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
