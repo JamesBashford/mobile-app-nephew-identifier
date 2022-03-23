e738d692a4e586975ea7d6255ec714e27f1d21d0
JamesBashford <66199723+JamesBashford@users.noreply.github.com>
2022-03-23 18:46:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ pip install python-telegram-bot
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from fastai.vision.all import load_learner
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -55,15 +57,20 @@ def detect_mask(update, context):
 
 def main():
     load_model()
-    updater = Updater(token="5105937010:AAGq0X4gqd6n_wMjzf8SMUPd-TaMSQcbdbw
-", use_context=True)
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
 
     dp.add_handler(MessageHandler(Filters.photo, detect_mask))
 
-    updater.start_polling()
+    dp.add_error_handler(error)
+    
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    
     updater.idle()
 
 
