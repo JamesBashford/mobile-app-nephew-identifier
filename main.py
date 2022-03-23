99670fcb1f18fb41270897bc00982c22485d7b94
JamesBashford <66199723+JamesBashford@users.noreply.github.com>
2022-03-23 18:49:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://mikey-danny-identifier.herokuapp.com/' + TOKEN)
     
     updater.idle()
 
